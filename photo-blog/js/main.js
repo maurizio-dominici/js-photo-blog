35209ab3d6bfe4fb0ea7b1f56ff95f05db1e5c60
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-28 16:41:56 +0200

    add layover

--- photo-blog/js/main.js
+++ photo-blog/js/main.js
@@ -3,16 +3,17 @@
 const generatePostCard = (post) => {
   const cardHtml = `
    <div class="col-12 col-md-6 col-lg-4">
-    <div class="card my-5">
+    <div class="card my-5" id="post-card-${post.id}">
        <img src="../img/pin.svg" alt="red-pin" class="red-pin" />
-       <div class="card-header">
-         <img src="${post.url}" alt="${post.title}"/>
+       <div class="card-header" id="card-header-${post.id}">
+         <img id="image-big-layover" src="${post.url}" alt="${post.title}"/>
        </div>
        <div class="card-body">
-         <span>
+         <span class="date">
            ${post.date}
          </span>
-         <span>
+         <br>
+         <span class="title">
            ${post.title}
          </span>
       </div>
@@ -27,6 +28,8 @@ const generatePostCard = (post) => {
 
 const gridPostEl = document.getElementById("grid-post");
 
+// ^ SVOLGIMENTO
+
 apiUri = "https://lanciweb.github.io/demo/api/pictures/";
 
 axios.get(apiUri).then((response) => {
@@ -34,8 +37,19 @@ axios.get(apiUri).then((response) => {
   console.log(posts);
 
   let cardsHtml = "";
+
   posts.forEach((post) => {
     cardsHtml += generatePostCard(post);
   });
+
   gridPostEl.innerHTML += cardsHtml;
+
+  const postsNodes = document.querySelectorAll(".card-header");
+
+  postsNodes.forEach((postNode) => {
+    postNode.addEventListener("click", () => {
+      const imageSrc = document.querySelector(`#${postNode.id} img`).src;
+      console.log(imageSrc);
+    });
+  });
 });
