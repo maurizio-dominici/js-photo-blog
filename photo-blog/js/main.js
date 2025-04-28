855e9283a36e6fcf8e43e1e14e7dd3b8a0ae1741
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-28 12:07:59 +0200

    add cards in js

--- photo-blog/js/main.js
+++ photo-blog/js/main.js
@@ -1,4 +1,31 @@
+// ^ FUNZIONI
+
+const generatePostCard = (post) => {
+  const cardHtml = `
+   <div class="col-12 col-md-6 col-lg-4">
+    <div class="card my-5">
+       <img src="../img/pin.svg" alt="red-pin" class="red-pin" />
+       <div class="card-header">
+         <img src="${post.url}" alt="${post.title}"/>
+       </div>
+       <div class="card-body">
+         <span>
+           ${post.date}
+         </span>
+         <span>
+           ${post.title}
+         </span>
+      </div>
+    </div>
+   </div>
+  `;
+
+  return cardHtml;
+};
+
 // ^ DATI
+
+const gridPostEl = document.getElementById("grid-post");
 
 apiUri = "https://lanciweb.github.io/demo/api/pictures/";
 
@@ -6,8 +33,9 @@ axios.get(apiUri).then((response) => {
   const posts = response.data;
   console.log(posts);
 
-  for (let i = 0; i < posts.length; i++) {
-    const currentPost = posts[i];
-    console.log(currentPost);
-  }
+  let cardsHtml = "";
+  posts.forEach((post) => {
+    cardsHtml += generatePostCard(post);
+  });
+  gridPostEl.innerHTML += cardsHtml;
 });
